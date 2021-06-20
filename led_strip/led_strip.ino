1e2f332565275fb24635ef4874589ce2b3cd2b90
Roman <pirogovrv@gmail.com>
2021-06-20 23:02:29 +0300

    color editor / reader. support for 64 saved in eeprom chip0 colors

--- led_strip/led_strip.ino
+++ led_strip/led_strip.ino
@@ -26,13 +26,17 @@
 #define MAX_LED_NUM 225
 #define MAX_IMG_LEN 225
 
+#define PATTERN_MAX_COUNT_EEPROM0 4
 #define PATTERN_MAX_INDEX 3 // Pattern count - 1
+
+#define MODIFY_BUTTON_PIN A0
+#define AMP_METER_PIN A1
 
 LiquidCrystal lcd(LCD_RS, LCD_EN, LCD_D4, LCD_D5, LCD_D6, LCD_D7);
 Adafruit_NeoPixel preview_led_array(LED_PREVIEW_ARRAY_PIXEL_NUM, LED_PREVIEW_ARRAY_PIN, NEO_GRB + NEO_KHZ800);
 
-enum stateType {patternSelect, colorSelect, validate, pause, resumeBack};
-enum buttonType {selectButton, actionButton};
+enum stateType {patternSelect, colorSelect, editMainRColor, editMainGColor, editMainBColor, editSecondaryRColor, editSecondaryGColor, editSecondaryBColor, validate, pause, resumeBack};
+enum buttonType {selectButton, actionButton, modifyButton};
 
 volatile enum stateType state = patternSelect; 
 
@@ -59,7 +63,10 @@ volatile byte secondary_blue = 0x00;
 volatile char secondary_color_hex[7];
 
 volatile bool color_preview = false;
-
+volatile int color_modify_reading_count = 0;
+volatile int color_up_momentum = 0;
+volatile int color_down_momentum = 0;
+volatile byte color_index = 0;
 
 void setup() 
 {
@@ -91,7 +98,6 @@ void loop()
 {
   if(isSelectPressed)
   {
-    digitalWrite(LED_BUILTIN, HIGH);
     transitState(selectButton);
     isSelectPressed = false;
   }
@@ -102,18 +108,34 @@ void loop()
     isActionPressed = false;
   }
 
+  processModifyButton();
+  if(color_modify_reading_count == 10)
+  {
+    color_modify_reading_count = 0;
+    transitState(modifyButton);
+  }
+
   render();
 
   if(state == patternSelect)
     preview();
 
-  if(state != colorSelect && color_preview)
+  if((state == patternSelect || state == validate || state == pause || state == resumeBack) && color_preview)
   {
     color_preview = false;
-    cancel_PreviewColors();
+    cancelPreviewColors();
   }    
 
   delay(100);
+}
+
+void processModifyButton()
+{
+  int modify_button_reading = analogRead(MODIFY_BUTTON_PIN);
+  if(modify_button_reading < 256)
+    color_modify_reading_count++;
+  else
+    color_modify_reading_count = 0;
 }
 
 void preview()
@@ -147,8 +169,8 @@ void transitState(enum buttonType button)
           readCurrentPatternPreview(pattern_index, pattern_preview_line_offset);
           break;
         case actionButton:
-          //select current pattern
           state = colorSelect;
+          readColors();
           break;
       }
       break;
@@ -156,11 +178,99 @@ void transitState(enum buttonType button)
       switch(button)
       {
         case selectButton:
-          //show next colors
+          color_index = color_index == 63 ? 0 : color_index + 1;
+          readColors();
           break;
         case actionButton:
-          //apply schema
           state = validate;
+          break;
+        case modifyButton:
+          state = editMainRColor;
+          break;
+      }
+      break;
+    case editMainRColor:
+      switch(button)
+      {
+        case selectButton:
+          main_red = colorDown(main_red);
+          break;
+        case actionButton:
+          main_red = colorUp(main_red);
+          break;
+        case modifyButton:
+          state = editMainGColor;
+          break;
+      }
+      break;
+    case editMainGColor:
+      switch(button)
+      {
+        case selectButton:
+          main_green = colorDown(main_green);
+          break;
+        case actionButton:
+          main_green = colorUp(main_green);
+          break;
+        case modifyButton:
+          state = editMainBColor;
+          break;
+      }
+      break;
+    case editMainBColor:
+      switch(button)
+      {
+        case selectButton:
+          main_blue = colorDown(main_blue);
+          break;
+        case actionButton:
+          main_blue = colorUp(main_blue);
+          break;
+        case modifyButton:
+          state = editSecondaryRColor;
+          break;
+      }
+      break;
+    case editSecondaryRColor:
+      switch(button)
+      {
+        case selectButton:
+          secondary_red = colorDown(secondary_red);
+          break;
+        case actionButton:
+          secondary_red = colorUp(secondary_red);
+          break;
+        case modifyButton:
+          state = editSecondaryGColor;
+          break;
+      }
+      break;
+    case editSecondaryGColor:
+      switch(button)
+      {
+        case selectButton:
+          secondary_green = colorDown(secondary_green);
+          break;
+        case actionButton:
+          secondary_green = colorUp(secondary_green);
+          break;
+        case modifyButton:
+          state = editSecondaryBColor;
+          break;
+      }
+      break;
+    case editSecondaryBColor:
+      switch(button)
+      {
+        case selectButton:
+          secondary_blue = colorDown(secondary_blue);
+          break;
+        case actionButton:
+          secondary_blue = colorUp(secondary_blue);
+          break;
+        case modifyButton:
+          state = colorSelect;
+          writeColors();
           break;
       }
       break;
@@ -193,6 +303,20 @@ void transitState(enum buttonType button)
   }
 }
 
+byte colorUp(byte color)
+{
+  color_up_momentum++;
+  color_down_momentum = 0;
+  return color = color == 0xFF ? 0x00 : color + color_up_momentum;
+}
+
+byte colorDown(byte color)
+{
+  color_down_momentum++;
+  color_up_momentum = 0;
+  return color = color == 0x00 ? 0xFF : color - color_down_momentum;
+}
+
 void render() 
 {
   lcd.setCursor(0, 0);
@@ -213,16 +337,53 @@ void render()
     case colorSelect:
       setColorStrings();
       previewColors();
-      lcd.print("Choose colors       ");
-      lcd.setCursor(0, 1);
-      lcd.print("RGB M: ");
-      lcd.print((char*)main_color_hex);
-      lcd.print("       ");
-      lcd.setCursor(0, 2);
-      lcd.print("RGB S: ");
-      lcd.print((char*)secondary_color_hex);
-      lcd.print("       ");
-      renderChoice("Select          Next");
+      lcd.print("Choose colors   [");
+      printNumber(color_index);
+      lcd.print("]");
+      displayColorStrings();
+      renderChoice("Select  Modify  Next");
+      break;
+    case editMainRColor:
+      setColorStrings();
+      previewColors();
+      lcd.print("Set main red        ");
+      displayColorStrings();
+      renderChoice("-    Main green    +");
+      break;
+    case editMainGColor:
+      setColorStrings();
+      previewColors();
+      lcd.print("Set main green      ");
+      displayColorStrings();
+      renderChoice("-    Main blue     +");    
+      break;
+    case editMainBColor:
+      setColorStrings();
+      previewColors();
+      lcd.print("Set main blue       ");
+      displayColorStrings();
+      renderChoice("-  Secondary red   +");    
+      break;
+    case editSecondaryRColor:
+      setColorStrings();
+      previewColors();
+      lcd.print("Set secondary red   ");
+      displayColorStrings();
+      renderChoice("- Secondary green  +");
+      break;
+    case editSecondaryGColor:
+      setColorStrings();
+      previewColors();
+      lcd.print("Set secondary green ");
+      displayColorStrings();
+      renderChoice("-  Secondary blue  +");    
+      break;
+    case editSecondaryBColor:
+      setColorStrings();
+      previewColors();
+      lcd.print("Set secondary blue  ");
+      displayColorStrings();
+      renderChoice("-      Apply       +");    
       break;
     case validate:
       lcd.print("PATTERN: ");
@@ -260,21 +421,62 @@ void render()
   } 
 }
 
+void printNumber(byte number)
+{
+  if(number < 10)
+    lcd.print(" ");
+  lcd.print(number);
+}
+
+void displayColorStrings()
+{
+  lcd.setCursor(0, 1);
+  lcd.print("RGB M: ");
+  lcd.print((char*)main_color_hex);
+  lcd.print("       ");
+  lcd.setCursor(0, 2);
+  lcd.print("RGB S: ");
+  lcd.print((char*)secondary_color_hex);
+  lcd.print("       ");
+}
+
 void previewColors()
 {
-   preview_led_array.setPixelColor(0, preview_led_array.Color(main_red, main_green, main_blue));
-   preview_led_array.setPixelColor(1, preview_led_array.Color(secondary_red, secondary_green, secondary_blue));
-   preview_led_array.show();
-   color_preview = true;
+  preview_led_array.setPixelColor(0, preview_led_array.Color(main_red, main_green, main_blue));
+  preview_led_array.setPixelColor(1, preview_led_array.Color(secondary_red, secondary_green, secondary_blue));
+  preview_led_array.show();
+  color_preview = true;
 }
 
-void cancel_PreviewColors()
+void cancelPreviewColors()
 {
-   preview_led_array.setPixelColor(0, preview_led_array.Color(0x00, 0x00, 0x00));
-   preview_led_array.setPixelColor(1, preview_led_array.Color(0x00, 0x00, 0x00));
-   preview_led_array.show();
+  preview_led_array.setPixelColor(0, preview_led_array.Color(0x00, 0x00, 0x00));
+  preview_led_array.setPixelColor(1, preview_led_array.Color(0x00, 0x00, 0x00));
+  preview_led_array.show();
 }
 
+void readColors()
+{
+  int baseAddress = PATTERN_MAX_COUNT_EEPROM0 * 32 * (MAX_IMG_LEN + 1) + 6 * color_index; // 4 x schema + RGBRGB x index
+  main_red = readEEPROM(EEPROM0_ADDRESS, baseAddress++);
+  main_green = readEEPROM(EEPROM0_ADDRESS, baseAddress++);
+  main_blue = readEEPROM(EEPROM0_ADDRESS, baseAddress++);
+  secondary_red = readEEPROM(EEPROM0_ADDRESS, baseAddress++);
+  secondary_green = readEEPROM(EEPROM0_ADDRESS, baseAddress++);
+  secondary_blue = readEEPROM(EEPROM0_ADDRESS, baseAddress++);
+}
+
+void writeColors()
+{
+  int baseAddress = PATTERN_MAX_COUNT_EEPROM0 * 32 * (MAX_IMG_LEN + 1) + 6 * color_index; // see above ^^
+  
+  writeEEPROM(EEPROM0_ADDRESS, baseAddress++, main_red);
+  writeEEPROM(EEPROM0_ADDRESS, baseAddress++, main_green);
+  writeEEPROM(EEPROM0_ADDRESS, baseAddress++, main_blue);
+  writeEEPROM(EEPROM0_ADDRESS, baseAddress++, secondary_red);
+  writeEEPROM(EEPROM0_ADDRESS, baseAddress++, secondary_green);
+  writeEEPROM(EEPROM0_ADDRESS, baseAddress++, secondary_blue);
+}
 
 void printIterationNumber()
 {
@@ -296,7 +498,7 @@ void printIterationNumber()
 }
 
 void setColorStrings()
-{
+{  
   byte color_main_bytes[3] = {main_red, main_green, main_blue};
   byte color_secondary_bytes[3] = {secondary_red, secondary_green, secondary_blue};
   arrayToString(color_main_bytes, 3, main_color_hex);
@@ -305,14 +507,14 @@ void setColorStrings()
 
 void arrayToString(byte array[], unsigned int len, char buffer[])
 {
-    for (unsigned int i = 0; i < len; i++)
-    {
-        byte nib1 = (array[i] >> 4) & 0x0F;
-        byte nib2 = (array[i] >> 0) & 0x0F;
-        buffer[i*2+0] = nib1  < 0xA ? '0' + nib1  : 'A' + nib1  - 0xA;
-        buffer[i*2+1] = nib2  < 0xA ? '0' + nib2  : 'A' + nib2  - 0xA;
-    }
-    buffer[len*2] = '\0';
+  for (unsigned int i = 0; i < len; i++)
+  {
+    byte nib1 = (array[i] >> 4) & 0x0F;
+    byte nib2 = (array[i] >> 0) & 0x0F;
+    buffer[i*2+0] = nib1  < 0xA ? '0' + nib1  : 'A' + nib1  - 0xA;
+    buffer[i*2+1] = nib2  < 0xA ? '0' + nib2  : 'A' + nib2  - 0xA;
+  }
+  buffer[len*2] = '\0';
 }
 
 void renderChoice(char* bottomLine)
@@ -364,4 +566,15 @@ byte readEEPROM(int deviceaddress, unsigned int eeaddress)
     rdata = Wire.read();
 
   return rdata;
+}
+
+void writeEEPROM(int deviceaddress, unsigned int eeaddress, byte data) 
+{
+  Wire.beginTransmission(deviceaddress);
+  Wire.write((int)(eeaddress >> 8));   // MSB
+  Wire.write((int)(eeaddress & 0xFF)); // LSB
+  Wire.write(data);
+  Wire.endTransmission();
+
+  delay(10);
 }
